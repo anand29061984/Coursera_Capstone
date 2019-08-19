48c72acc7663943a36593ca8e59b8b7f1cb6b5a6
anand29061984 <53630767+anand29061984@users.noreply.github.com>
2019-08-19 11:21:53 -0400

    Add files via upload

---
+++ AK_capstone_project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+
+#  This notebook will be used for the capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
